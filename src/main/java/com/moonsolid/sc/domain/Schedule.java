09bf8480a599b8aa604ba5db98fe8357b54224d0
MoonSolid <rnreo20@gmail.com>
2020-03-11 10:54:56 +0900

    v032_4_1

--- src/main/java/com/moonsolid/sc/domain/Schedule.java
+++ src/main/java/com/moonsolid/sc/domain/Schedule.java
@@ -4,24 +4,32 @@ import java.io.Serializable;
 
 public class Schedule implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 20200311L;
+
   int No;
   String place;
   String description;
   String memo;
   String cost;
+  String scheduleTime;
 
+  @Override
+  public String toString() {
+    return "Schedule [No=" + No + ", place=" + place + ", description=" + description + ", memo="
+        + memo + ", cost=" + cost + ", scheduleTime=" + scheduleTime + "]";
+  }
 
 
   @Override
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((description == null) ? 0 : description.hashCode());
     result = prime * result + No;
     result = prime * result + ((cost == null) ? 0 : cost.hashCode());
+    result = prime * result + ((description == null) ? 0 : description.hashCode());
     result = prime * result + ((memo == null) ? 0 : memo.hashCode());
     result = prime * result + ((place == null) ? 0 : place.hashCode());
+    result = prime * result + ((scheduleTime == null) ? 0 : scheduleTime.hashCode());
     return result;
   }
 
@@ -34,17 +42,17 @@ public class Schedule implements Serializable {
     if (getClass() != obj.getClass())
       return false;
     Schedule other = (Schedule) obj;
-    if (description == null) {
-      if (other.description != null)
-        return false;
-    } else if (!description.equals(other.description))
-      return false;
     if (No != other.No)
       return false;
     if (cost == null) {
       if (other.cost != null)
         return false;
     } else if (!cost.equals(other.cost))
+      return false;
+    if (description == null) {
+      if (other.description != null)
+        return false;
+    } else if (!description.equals(other.description))
       return false;
     if (memo == null) {
       if (other.memo != null)
@@ -56,8 +64,14 @@ public class Schedule implements Serializable {
         return false;
     } else if (!place.equals(other.place))
       return false;
+    if (scheduleTime == null) {
+      if (other.scheduleTime != null)
+        return false;
+    } else if (!scheduleTime.equals(other.scheduleTime))
+      return false;
     return true;
   }
+
 
 
   public int getNo() {
@@ -81,7 +95,7 @@ public class Schedule implements Serializable {
   }
 
   public void setDescription(String description) {
-    description = description;
+    this.description = description;
   }
 
   public String getMemo() {
@@ -99,6 +113,15 @@ public class Schedule implements Serializable {
   public void setCost(String cost) {
     this.cost = cost;
   }
+
+  public String getScheduleTime() {
+    return scheduleTime;
+  }
+
+  public void setScheduleTime(String scheduleTime) {
+    this.scheduleTime = scheduleTime;
+  }
+
 
 
 }
